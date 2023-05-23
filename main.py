97efef8df6d20acaa459b26cb8bec8ba2b6e9bc7
Elyza <elyzajanerelucio@gmail.com>
2023-05-23 19:53:00 +0800

    Def function for the operation

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from addition import Addition
 from subtraction import Subtraction
 from multiplication import Multiplication
 from division import Division
+
+def operate(num1, num2, operation):
+    if operation == 1:
+        return Addition.execute(num1, num2)
+    elif operation == 2:
+        return Subtraction.execute(num1, num2)
+    elif operation == 3:
+        return Multiplication.execute(num1, num2)
+    elif operation == 4:
+        return Division.execute(num1, num2)
+    else:
+        raise ValueError("Invalid operation!")
