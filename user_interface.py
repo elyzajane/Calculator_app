953b8fa9c02edddd39ea7e9fa37574a6df473898
Elyza <elyzajanerelucio@gmail.com>
2023-05-23 21:42:50 +0800

    Fix the error, finalize the code and add designs

--- user_interface.py
+++ user_interface.py
@@ -4,36 +4,36 @@ class UserInterface:
         print("\033[1;35;40m╦ ╦ ╦═╗╦  ╦  ╔═╗")
         print("\033[1;35;40m╠═╣ ╠═ ║  ║  ║ ║")
         print("\033[1;35;40m╩ ╩ ╩═╝╩═╝╩═╝╚═╝")
-        
+
     def display_warning():
-        print("Kindly use the calculator app wisely!")
+        print("\033[1;31;40mKindly use the calculator app wisely!\033[0m")
 
     def display_options():
-        print("Please choose the operation.")
-        print("1. Addition")
-        print("2. Subtraction")
-        print("3. Multiplication")
-        print("4. Division")
+        print("\033[1;33;40mPlease choose the operation.\033[0m")
+        print("\033[1;32;40m1. Addition\033[0m")
+        print("\033[1;36;40m2. Subtraction\033[0m")
+        print("\033[1;34;40m3. Multiplication\033[0m")
+        print("\033[1;35;40m4. Division\033[0m")
 
     def get_operation_choice():
         while True:
             try:
-                operation = int(input("Enter your chosen operation (1/2/3/4):"))
+                operation = int(input("\033[1;33;40mEnter your chosen operation (1/2/3/4): \033[0m"))
                 if operation < 1 or operation > 4:
                     raise ValueError
                 break
             except ValueError:
-                print("An error occurred! Please enter a valid choice (1/2/3/4).")
+                print("\033[1;31;40mAn error occurred! Please enter a valid choice (1/2/3/4).\033[0m")
         return operation
     
     def get_numbers():
         while True:
             try:
-                num1 = float(input("Enter first number:"))
-                num2 = float(input("Enter second number:"))
+                num1 = float(input("\033[1;33;40mEnter first number: \033[0m"))
+                num2 = float(input("\033[1;33;40mEnter second number: \033[0m"))
                 break
             except ValueError:
-                print("An error occurred! Please enter valid numbers.")
+                print("\033[1;31;40mAn error occurred! Please enter valid numbers.\033[0m")
         return num1, num2
     
     def display_result(num1, operator_symbol, num2, result):
