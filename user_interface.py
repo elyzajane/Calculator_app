76fb5aa91fc9a12eaa5181236ebf3e9cb8be7d9e
Elyza <elyzajanerelucio@gmail.com>
2023-05-23 16:27:59 +0800

    Display the operations

--- user_interface.py
+++ user_interface.py
@@ -2,3 +2,10 @@ class UserInterface:
 
     def display_warning():
         print("Kindly use the calculator app wisely!")
+
+    def display_options():
+        print("Please choose the operation.")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
