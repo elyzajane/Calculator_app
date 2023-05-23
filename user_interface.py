6e73ca4020110a13910bc4e9e7644a22e04a9eac
Elyza <elyzajanerelucio@gmail.com>
2023-05-23 17:44:53 +0800

    Get the chosen operation by the user

--- user_interface.py
+++ user_interface.py
@@ -9,3 +9,15 @@ class UserInterface:
         print("2. Subtraction")
         print("3. Multiplication")
         print("4. Division")
+
+    def get_operation_choice():
+        while True:
+            try:
+                operation = int(input("Enter your chosen operation (1/2/3/4):"))
+                if operation < 1 or operation > 4:
+                    raise ValueError
+                break
+            except ValueError:
+                print("An error occurred! Please enter a valid choice (1/2/3/4):")
+        return operation
+    
