e9a1644b7ae80b3b3c7e1465c4ee1bdb3d5582b2
Elyza <elyzajanerelucio@gmail.com>
2023-05-23 19:51:34 +0800

    Execute the operations and user interface

---
+++ main.py
@@ -0,0 +1,5 @@
+from user_interface import UserInterface
+from addition import Addition
+from subtraction import Subtraction
+from multiplication import Multiplication
+from division import Division
